0b502a5fc5f02a788c5412e4c830da9b43b9a196
avalentini <antonio.valentini@live.it>
2020-06-01 11:02:19 +0200

    Basic project structure

---
+++ src/Avalentini.Shakesmon.Api/Controllers/PokemonController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Avalentini.Shakesmon.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PokemonController : ControllerBase
+    {
+        [HttpGet("{name}")]
+        public async Task<IActionResult> Get(string name)
+        {
+            await Task.CompletedTask;
+            return Ok(new GetDto
+            {
+                Name = name,
+                Description = "some description",
+            });
+        }
+    }
+
+    public class GetDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
